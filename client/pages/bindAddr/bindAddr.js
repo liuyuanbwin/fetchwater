d2dc4d898bfed93603bfa949cd020ca1b40d8e65
LeoLau <liuyuansix@163.com>
2018-12-03 19:28:18 +0800

    新建页面和数据库

--- client/pages/bindAddr/bindAddr.js
+++ client/pages/bindAddr/bindAddr.js
@@ -1,10 +1,10 @@
+// client/pages/bindAddr/bindAddr.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    tel:''
 
   },
 
@@ -12,67 +12,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
-  },
 
-  //绑定输入框内容
-  watchTel:function(e){
-    console.log('tel ' + JSON.stringify(e.detail.detail.value))
-    this.setData({
-      tel:e.detail.detail.value
-    })
-  },
-  handleClick:function(){
-    console.log('tel' + this.data.tel);
   }
-
 })
--- server/tools/cAuth.sql
+++ server/tools/cAuth.sql
@@ -1,21 +1,83 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : Localhost
+ Source Server         : local
  Source Server Type    : MySQL
- Source Server Version : 50717
+ Source Server Version : 50635
  Source Host           : localhost
  Source Database       : cAuth
 
  Target Server Type    : MySQL
- Target Server Version : 50717
+ Target Server Version : 50635
  File Encoding         : utf-8
 
- Date: 08/10/2017 22:22:52 PM
+ Date: 12/03/2018 19:23:35 PM
 */
 
-SET NAMES utf8;
+SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `_mysql_session_store`
+-- ----------------------------
+DROP TABLE IF EXISTS `_mysql_session_store`;
+CREATE TABLE `_mysql_session_store` (
+  `id` varchar(255) NOT NULL,
+  `expires` bigint(20) DEFAULT NULL,
+  `data` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `addr`
+-- ----------------------------
+DROP TABLE IF EXISTS `addr`;
+CREATE TABLE `addr` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `openId` varchar(100) NOT NULL,
+  `province` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `area` varchar(100) NOT NULL,
+  `detail` varchar(100) NOT NULL,
+  `default` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `bindName`
+-- ----------------------------
+DROP TABLE IF EXISTS `bindName`;
+CREATE TABLE `bindName` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nickName` varchar(30) DEFAULT NULL,
+  `interName` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `bindName`
+-- ----------------------------
+BEGIN;
+INSERT INTO `bindName` VALUES ('1', '弄死', '你了'), ('2', '昵称', '内部名');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `bindTel`
+-- ----------------------------
+DROP TABLE IF EXISTS `bindTel`;
+CREATE TABLE `bindTel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `openId` varchar(100) NOT NULL,
+  `tel` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `bindTel`
+-- ----------------------------
+BEGIN;
+INSERT INTO `bindTel` VALUES ('1', '999', '13831217198');
+COMMIT;
 
 -- ----------------------------
 --  Table structure for `cSessionInfo`
@@ -33,5 +95,54 @@ CREATE TABLE `cSessionInfo` (
   KEY `openid` (`open_id`) USING BTREE,
   KEY `skey` (`skey`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='会话管理用户信息';
+
+-- ----------------------------
+--  Records of `cSessionInfo`
+-- ----------------------------
+BEGIN;
+INSERT INTO `cSessionInfo` VALUES ('ooyfE5EwYz4SyNg0FyL8X9msSdOc', 'd1f31e76-5ffa-4c3c-beba-eee64605a2ba', 'b27f8681a8875dc151bb4b580329df3084dc2540', '2018-12-01 19:39:55', '2018-12-02 13:04:47', 'i9Prat0dN3k+35ci/778YQ==', '{\"openId\":\"ooyfE5EwYz4SyNg0FyL8X9msSdOc\",\"nickName\":\"锦衣夜行\",\"gender\":1,\"language\":\"zh_CN\",\"city\":\"\",\"province\":\"Beijing\",\"country\":\"China\",\"avatarUrl\":\"https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eoic7cL9EMtiaTybFJAMDYlSibhdcyHysLpndKK807WicMGHMjbaVjNBgGVRxca7dha6nib0PrC6nsvcLQ/132\",\"watermark\":{\"timestamp\":1543727086,\"appid\":\"wx93341736c68e3616\"}}');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `cart`
+-- ----------------------------
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `waresId` varchar(100) NOT NULL COMMENT '商品编号',
+  `count` int(11) NOT NULL DEFAULT '1' COMMENT '数量',
+  `mark` varchar(100) NOT NULL COMMENT '备注'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `posts`
+-- ----------------------------
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `content` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `posts`
+-- ----------------------------
+BEGIN;
+INSERT INTO `posts` VALUES ('1', '1', 'sfasfasfasf');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `wares`
+-- ----------------------------
+DROP TABLE IF EXISTS `wares`;
+CREATE TABLE `wares` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `detail` varchar(100) DEFAULT NULL,
+  `mark` varchar(100) DEFAULT NULL,
+  `discount` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
